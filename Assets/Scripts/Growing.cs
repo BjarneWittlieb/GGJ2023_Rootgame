cc68caa411c59f411700895aa028820d2a47dc22
Nicolas Harttig <nicolas_harttig@hotmail.de>
2023-02-03 22:09:46 +0100

    branching is possible now

--- Assets/Scripts/Growing.cs
+++ Assets/Scripts/Growing.cs
@@ -9,40 +9,61 @@ public class Growing : MonoBehaviour
     [SerializeField] private float SplitDistance = 2;
 
     private RootNode node;
+    private IsRootTip tip;
 
     // Start is called before the first frame update
     void Start()
     {
         node = GetComponent<RootNode>();
+        tip = GetComponent<IsRootTip>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        checkSplit();
+        if (!node.Parent)
+            return;
+        if (tip.IsTip && node.Children.Count == 0)
+            split();
+        else if (tip.IsTip && node.Children.Count != 0)
+            branch();
     }
 
-    void checkSplit() {
-        if (node.Parent == null)
-            return;
-        var next = node.Parent.transform.position;
-        var me = transform.position;
-        if ((next-me).magnitude > SplitDistance) {
-            split();
-        }
+    bool distanceToHigh(RootNode a, RootNode b) {
+        return (a.transform.position - b.transform.position).magnitude > SplitDistance;
     }
 
     void split() {
-        RootNode parent = node.Parent;
-        parent.Children.Remove(node);        
+        if (node.Parent == null)
+            return;
+        if (distanceToHigh(node.Parent, node)) {
+            split(node);
+        }
+    }
+    void branch() {
+        var parent = node.Parent;
+        parent.Children.Remove(node);
         var newNodeObj = Instantiate(transform.gameObject, null);
         var newNode = newNodeObj.GetComponent<RootNode>();
+        newNode.Children = node.Children;
+        foreach (var x in node.Children)
+            x.Parent = newNode;
+        newNode.Children.Add(node);
+        node.Parent = newNode;
+        node.Children.Clear();
+    }
+
+    void split(RootNode Child) {
+        RootNode parent = Child.Parent;
+        parent.Children.Remove(Child);        
+        var newNodeObj = Instantiate(transform.gameObject, null);
+        var newNode = newNodeObj.GetComponent<RootNode>();
+        newNodeObj.GetComponent<IsRootTip>().IsTip = false;
         newNode.Children.Clear();        
         newNode.Parent = parent;
         parent.Children.Add(newNode);
-        newNode.Children.Add(node);
-        node.Parent = newNode;
-
-        newNode.transform.position = (node.transform.position + parent.transform.position) / 2;
+        newNode.Children.Add(Child);
+        Child.Parent = newNode;
+        newNode.transform.position = (Child.transform.position + parent.transform.position) / 2;
     }
 }

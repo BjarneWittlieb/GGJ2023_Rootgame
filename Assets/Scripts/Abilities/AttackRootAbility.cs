f7b0c0f48954f15548d2abc97ac226221228959a
Gordon <gordon_bergmann@gmx.net>
2023-02-04 20:45:15 +0100

    1

---
+++ Assets/Scripts/Abilities/AttackRootAbility.cs
@@ -0,0 +1,34 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+
+namespace Abilities
+{
+    public class AttackRootAbility : BaseAbility
+    {
+        private NodePicker picker;
+        public void OnEnable()
+        {
+            picker           = GameObject.Find("Player").GetComponent<NodePicker>();
+            Cooldown         = 1;
+            CooldownTimeLeft = 1;
+            RequiredResource = ScriptableObject.CreateInstance<Mana>();
+            ResourceCost     = 40;
+        }
+        
+        public override void Execute(AbilityHolder holder)
+        {
+            Attack();
+        }
+        
+        private void Attack()
+        {
+            var rootTarget = picker.PickTarget();
+            
+            if (rootTarget)
+            {
+                if (rootTarget.GetComponent<RootAttack>())
+                    rootTarget.GetComponent<RootAttack>().attack(picker.marker.transform.position);
+            }
+        }
+    }
+}

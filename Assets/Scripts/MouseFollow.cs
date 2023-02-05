044bd9b345825f0343df1990ae8554e3b92d8d36
Armbrust, Cornelius <Cornelius.Armbrust@adesso.de>
2023-02-05 08:59:21 +0100

    haben > brauchen

---
+++ Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MouseFollow : MonoBehaviour
+    {
+        private Camera camera1;
+
+        private void Start()
+        {
+            camera1 = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (!camera1)
+                return;
+
+            var mousePosition = camera1.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z    = camera1.transform.position.z + camera1.nearClipPlane;
+            transform.position = mousePosition;
+        }
+    }
+}

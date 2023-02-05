d090d76ee67e24f887f851b3b1e492f2f7abac4b
Bjarne Wittlieb <bjarne.wittlieb@adesso.de>
2023-02-05 06:27:32 +0100

    [Bjarne] Root now grows in direction of mouse when nearby and stamina system implemented

---
+++ Assets/GrowDirectionController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GrowDirectionController : MonoBehaviour
+{
+    private RandomWalk _randomWalk;
+
+    public float mouseInfluenceRadius = 3f;
+
+    public float influenceMinValue = 0.05f;
+    public float influenceMaxValue = .2f;
+
+    public float totalStamina = .5f;
+
+    private Camera cam;
+
+    private float _staminaLeft = 0f;
+
+    private bool _clickedBefore = false;
+
+    public float onChargeRadiusMultiplier = 5f;
+    public float onChargeSpeedMultiplier = 2f;
+
+    private float _currentRadiusMultiplier = 1f;
+    private float _currentSpeedMultiplier = 1f;
+
+    private float _idleWalkSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _randomWalk = GetComponent<RandomWalk>();
+        cam = Camera.main;
+
+        _idleWalkSpeed = _randomWalk.walkSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessINputAndStamina();
+
+        var mousePos = Vector3.Scale(cam.ScreenToWorldPoint(Input.mousePosition), new Vector3(1, 1, 0));
+
+        if ((mousePos - transform.position).magnitude > mouseInfluenceRadius * _currentRadiusMultiplier)
+        {
+            _randomWalk.growInGeneralDirection = false;
+            return;
+        }
+
+        _randomWalk.growInGeneralDirection = true;
+        _randomWalk.generalDirection = mousePos - transform.position;
+        _randomWalk.probabilityOfDirectionModifier = ((mousePos - transform.position).magnitude / mouseInfluenceRadius) * (influenceMaxValue - influenceMinValue);
+    }
+
+    void ProcessINputAndStamina()
+    {
+        _currentRadiusMultiplier = 1f;
+        _currentSpeedMultiplier = 1f;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!_clickedBefore)
+            {
+                _clickedBefore = true;
+            }
+            else
+            {
+                _staminaLeft = Mathf.Max(_staminaLeft - Time.deltaTime, -0.01f);
+                if (_staminaLeft > 0)
+                {
+                    _currentRadiusMultiplier = onChargeRadiusMultiplier;
+                    _currentSpeedMultiplier  = onChargeSpeedMultiplier;
+                }
+            }
+        }
+        else
+        {
+            if (_clickedBefore)
+            {
+                _clickedBefore = false;
+            }
+            else
+            {
+                _staminaLeft = Mathf.Min(_staminaLeft + Time.deltaTime / 3, totalStamina);
+            }
+        }
+
+        // TODO CHARGE HERE, speed has to be adjusted :)
+        _randomWalk.walkSpeed = _currentSpeedMultiplier * _idleWalkSpeed;
+    }
+}
--- Assets/Scripts/RandomWalk.cs
+++ Assets/Scripts/RandomWalk.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 [RequireComponent(typeof(IsRootTip))]
 [RequireComponent(typeof(RootNode))]
-public class RandomWalk : MonoBehaviour {
+public class RandomWalk : MonoBehaviour
+{
     [SerializeField] private float RotationSpeed = 0.1f;
     [SerializeField] private float rotationRange = Mathf.PI / 3;
     [SerializeField] private float progressChance = 0.01f;
-    public Vector2 gravity = new Vector2(0,-0.1f);
+    public Vector2 gravity = new Vector2(0, -0.1f);
 
-    [Range(0.01f,0.99f)]
-    [SerializeField] private float walkSpeed = 0.05f;
+    [Range(0.01f, 0.99f)]
+    [SerializeField] public float walkSpeed = 0.05f;
     private IsRootTip tip;
     private RootNode node;
 
-    private bool currentRotation = false;
+    public float probabilityOfDirectionModifier = .07f;
+
+    private bool isRotatingRight = false;
+    /// <summary>
+    /// Is either -rotationSpeed or +rotationSpeed
+    /// </summary>
     private float currentAngle = 0;
     private float currentDistance = 0;
     private bool isSplitting = false;
 
+    /// <summary>
+    /// The general direction in which the node will grow.
+    /// If empty the node will grow completely random.
+    /// </summary>
+    public Vector2 generalDirection = Vector2.left;
+
+    public bool growInGeneralDirection = true;
 
     float sinceWallIssue;
     bool hasWallIssue = false;
@@ -27,84 +40,141 @@ public class RandomWalk : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
-        tip  = GetComponent<IsRootTip>();
+        tip = GetComponent<IsRootTip>();
         node = GetComponent<RootNode>();
-        currentRotation = Random.value < 0.5;
+        isRotatingRight = Random.value < getProbabilityOfTurningRight();
         currentDistance = 0;
         node.OnSplit += splitting;
         if (!node.Parent)
-            return; 
+            return;
     }
 
-    void splitting() {
+    void splitting()
+    {
         currentDistance = tip.SplitDistance / 2;
         isSplitting = false;
-        currentRotation = Random.value < 0.5;
+        isRotatingRight = Random.value < getProbabilityOfTurningRight();
         currentAngle = 0;
+    }
+
+    /// <summary>
+    /// Returns the probabilty to turn right depending on the generalDirection vector
+    /// </summary>
+    /// <returns></returns>
+    private float getProbabilityOfTurningRight()
+    {
+        if (!growInGeneralDirection || !node.Parent)
+        {
+            return .5f;
+        }
+
+        float differenceOnLeftTurn = (getTargetDirection(-RotationSpeed).normalized - generalDirection.normalized).magnitude;
+        float differenceOnRightTurn = (getTargetDirection(RotationSpeed).normalized - generalDirection.normalized).magnitude;
+        return .5f + Mathf.Sign(differenceOnLeftTurn - differenceOnRightTurn) * probabilityOfDirectionModifier;
+    }
+
+    private Vector2 getParentDirection()
+    {
+        Vector2 parentDirection = new Vector2(0, 1);
+        bool hasDir = node.Parent.Parent;
+        Vector2 parentPosition = node.Parent.transform.position;
+        Vector2 parentParentPosition = parentPosition;
+        if (node.Parent.Parent)
+            parentParentPosition = node.Parent.Parent.transform.position;
+        if (node.IntermediatePoints.Count >= 1)
+        {
+            parentPosition = node.IntermediatePoints[node.IntermediatePoints.Count - 1];
+            parentParentPosition = node.Parent.transform.position;
+            hasDir = true;
+        }
+        if (node.IntermediatePoints.Count >= 2)
+        {
+            parentParentPosition = node.IntermediatePoints[node.IntermediatePoints.Count - 2];
+            hasDir = true;
+        }
+        if (hasDir)
+            parentDirection = (parentPosition - parentParentPosition).normalized;
+        return parentDirection;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if (tip.IsTip && node.Parent)
-            walk();        
+            walk();
     }
 
-    void walk() {
+    void walk()
+    {
         if (node.IsDead)
             return;
-        if (Random.value < progressChance) {
-            List<Collider2D> collider = new List<Collider2D>(Physics2D.OverlapPointAll(transform.position,LayerMask.GetMask("Wall")));
-            if (collider.Count == 0) {
+        if (Random.value < progressChance)
+        {
+            List<Collider2D> collider = new List<Collider2D>(Physics2D.OverlapPointAll(transform.position, LayerMask.GetMask("Wall")));
+            if (collider.Count == 0)
+            {
                 isSplitting = true;
                 hasWallIssue = false;
             }
-            else {
+            else
+            {
                 rotationRange *= 1.01f;
-                if (!hasWallIssue) {
+                if (!hasWallIssue)
+                {
                     hasWallIssue = true;
                     sinceWallIssue = Time.time;
                 }
-                else if ((Time.time-sinceWallIssue) > 5)
+                else if ((Time.time - sinceWallIssue) > 5)
                     node.IsDead = true;
             }
 
         }
         transform.position = getTargetPosition();
-        
-        if (currentAngle < -rotationRange && !currentRotation)
-            currentRotation = true;
-        if (currentAngle > rotationRange && currentRotation)
-            currentRotation = false;
-        currentAngle += (currentRotation ? 1 : -1) * RotationSpeed;
-        currentDistance = currentDistance * (1- walkSpeed) + tip.SplitDistance * (isSplitting?1.1f:0.9f) * walkSpeed;
+
+        if (currentAngle < -rotationRange && !isRotatingRight)
+            isRotatingRight = true;
+        if (currentAngle > rotationRange && isRotatingRight)
+            isRotatingRight = false;
+        currentAngle += (isRotatingRight ? 1 : -1) * RotationSpeed;
+        currentDistance = currentDistance * (1 - walkSpeed) + tip.SplitDistance * (isSplitting ? 1.1f : 0.9f) * walkSpeed;
 
     }
 
-    Vector2 getTargetPosition() {
+    private Vector2 getTargetDirection(float rotationAngle)
+    {
+        Vector2 parentDirection = getParentDirection();
+        float parentAngleOfDirection = Mathf.Deg2Rad * Vector2.SignedAngle(new Vector2(1, 0), parentDirection);
+        return new Vector2(Mathf.Cos(rotationAngle + parentAngleOfDirection), Mathf.Sin(rotationAngle + parentAngleOfDirection)).normalized * currentDistance + gravity;
+    }
+
+    Vector2 getTargetPosition()
+    {
         Vector2 parentDir = new Vector2(0, 1);
         bool hasDir = node.Parent.Parent;
         Vector2 parentPosition = node.Parent.transform.position;
         Vector2 parentParentPosition = parentPosition;
         if (node.Parent.Parent)
             parentParentPosition = node.Parent.Parent.transform.position;
-        if (node.IntermediatePoints.Count >= 1) {
+        if (node.IntermediatePoints.Count >= 1)
+        {
             parentPosition = node.IntermediatePoints[node.IntermediatePoints.Count - 1];
             parentParentPosition = node.Parent.transform.position;
             hasDir = true;
         }
-        if (node.IntermediatePoints.Count >= 2) {
+        if (node.IntermediatePoints.Count >= 2)
+        {
             parentParentPosition = node.IntermediatePoints[node.IntermediatePoints.Count - 2];
             hasDir = true;
         }
         if (hasDir)
             parentDir = (parentPosition - parentParentPosition).normalized;
-        var v = Mathf.Deg2Rad * Vector2.SignedAngle(new Vector2(1,0), parentDir);
+        var v = Mathf.Deg2Rad * Vector2.SignedAngle(new Vector2(1, 0), parentDir);
         Vector2 movementDir = new Vector2(Mathf.Cos(currentAngle + v), Mathf.Sin(currentAngle + v)).normalized * currentDistance + gravity;
         return parentPosition + movementDir;
     }
 
-    private void OnDrawGizmos() {
-        
+    private void OnDrawGizmos()
+    {
+
     }
 }
